516077300b4afea42898b5035b2f4adcb4c30fab
bhsohn <hanmuson1@gmail.com>
2025-08-14 18:40:27 +0900

    플레이어 이동 처리

--- Farming Game/Assets/Scripts/PlayerController.cs
+++ Farming Game/Assets/Scripts/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-                
+
     }
 
     // Update is called once per frame
@@ -20,6 +20,14 @@ public class PlayerController : MonoBehaviour
         //theRB.linearVelocity = new Vector2(moveSpeed, 0f);
         theRB.linearVelocity = moveInput.action.ReadValue<Vector2>().normalized * moveSpeed;
 
+        if (theRB.linearVelocity.x < 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (theRB.linearVelocity.x > 0f)
+        {
+            transform.localScale = Vector3.one;
+        }
 
         anim.SetFloat("speed", theRB.linearVelocity.magnitude);
     }
