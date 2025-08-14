1e9b469cf3b2032fce95ecdad0f059f0753779bc
bhsohn <hanmuson1@gmail.com>
2025-08-14 16:35:54 +0900

    PlayerController.cs commit

---
+++ Farming Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
