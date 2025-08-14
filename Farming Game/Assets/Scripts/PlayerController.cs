897dd69c1f6b3719d30773096e1b4104d2b418b1
bhsohn <hanmuson1@gmail.com>
2025-08-14 17:07:17 +0900

    캐릭터 이동

--- Farming Game/Assets/Scripts/PlayerController.cs
+++ Farming Game/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D theRB;
+    public float moveSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        theRB.linearVelocity = new Vector2(moveSpeed, 0f);
     }
 }
