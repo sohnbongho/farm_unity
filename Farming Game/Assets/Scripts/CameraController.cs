2d2f9e737fdc793817b7bd07f3bb2f0cd0c61c43
sonbongho <hanmuson1@gmail.com>
2025-08-17 22:44:12 +0900

    파란색 부분 안보이게 처리

--- Farming Game/Assets/Scripts/CameraController.cs
+++ Farming Game/Assets/Scripts/CameraController.cs
@@ -5,6 +5,8 @@ public class CameraController : MonoBehaviour
     private Transform target;
     public Transform clampMin;
     public Transform clampMax;
+    private Camera cam;
+    private float halfWidth, halfHeight;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,6 +14,12 @@ public class CameraController : MonoBehaviour
         target = FindAnyObjectByType<PlayerController>().transform;
         clampMin.SetParent(null);
         clampMax.SetParent(null);
+
+        cam = GetComponent<Camera>();
+
+        halfHeight = cam.orthographicSize;
+        halfWidth = cam.orthographicSize * cam.aspect;
+
     }
 
     // Update is called once per frame
@@ -20,8 +28,8 @@ public class CameraController : MonoBehaviour
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
 
         Vector3 clampedPosition = transform.position;
-        clampedPosition.x = Mathf.Clamp(clampedPosition.x, clampMin.position.x, clampMax.position.x);
-        clampedPosition.y = Mathf.Clamp(clampedPosition.y, clampMin.position.y, clampMax.position.y);
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, clampMin.position.x + halfWidth, clampMax.position.x - halfWidth);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, clampMin.position.y + halfHeight, clampMax.position.y - halfHeight);
 
         transform.position = clampedPosition;
 
