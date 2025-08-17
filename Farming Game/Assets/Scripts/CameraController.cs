1e9ef6fb6f5bf5092f71ebcf33f81d3bf7bff49d
sonbongho <hanmuson1@gmail.com>
2025-08-17 20:36:43 +0900

    카메라 최소, 최대 값까지만 이동하게 처리

--- Farming Game/Assets/Scripts/CameraController.cs
+++ Farming Game/Assets/Scripts/CameraController.cs
@@ -3,15 +3,27 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private Transform target;
+    public Transform clampMin;
+    public Transform clampMax;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         target = FindAnyObjectByType<PlayerController>().transform;
+        clampMin.SetParent(null);
+        clampMax.SetParent(null);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, clampMin.position.x, clampMax.position.x);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, clampMin.position.y, clampMax.position.y);
+
+        transform.position = clampedPosition;
+
     }
 }
