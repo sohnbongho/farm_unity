04d806b17386964a0faf72abc86fab0181b38659
bhsohn <hanmuson1@gmail.com>
2025-08-22 14:17:31 +0900

    최소 위치에 블럭 설치

---
+++ Farming Game/Assets/Scripts/GridController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public Transform minPoint, maxPoint;
+    public GrowBlock baseGridBlock;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateGrid()
+    {
+        minPoint.position = new Vector3(
+            Mathf.Round(minPoint.position.x),
+            Mathf.Round(minPoint.position.y),
+            0f);
+
+        maxPoint.position = new Vector3(
+            Mathf.Round(maxPoint.position.x),
+            Mathf.Round(maxPoint.position.y),
+            0f);
+
+        //Vector3 startpoint = minPoint.position + new Vector3(.5f, .5f, 0f);
+        Vector3 startpoint = minPoint.position + new Vector3(1f, 1f, 0f);
+
+        // 블럭의 복사본을 생성
+        Instantiate(baseGridBlock, startpoint, Quaternion.identity);
+
+    }
+}
