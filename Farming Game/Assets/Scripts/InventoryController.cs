89f21d6bb09ba822ed31e664f20919d2b8990f90
bhsohn <hanmuson1@gmail.com>
2025-08-27 18:48:39 +0900

    인벤토리 처리

--- Farming Game/Assets/Scripts/InventoryController.cs
+++ Farming Game/Assets/Scripts/InventoryController.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
+    public SeedDisplay[] seeds;
     public void OpenClose()
     {
         if(gameObject.activeSelf == false)
         {
             gameObject.SetActive(true);
+            UpdateDisplay();
         }
         else
         {
             gameObject.SetActive(false);
+        }
+    }
+
+    public void UpdateDisplay()
+    {
+        foreach(SeedDisplay seed in seeds)
+        {
+            seed.UpdateDisplay();
         }
     }
 
---
+++ Farming Game/Assets/Scripts/SeedDisplay.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SeedDisplay : MonoBehaviour
+{
+    public CropController.CropType crop;
+
+    public Image seedImage;
+    public TMP_Text seedAmount;
+
+    public void UpdateDisplay()
+    {
+        CropInfo info = CropController.instance.GetCropInfo(crop);
+
+        seedImage.sprite = info.seedType;
+        seedAmount.text = "x" + info.seedAmount;
+    }
+
+
+
+    public void SelectSeed()
+    {
+
+    }
+
+}
