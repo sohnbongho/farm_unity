4853604f57030869d4e5229c40911d541bd80050
bhsohn <hanmuson1@gmail.com>
2025-08-29 10:47:19 +0900

    seed 처리

--- Farming Game/Assets/Scripts/ShopController.cs
+++ Farming Game/Assets/Scripts/ShopController.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class ShopController : MonoBehaviour
 {
+    public ShopSeedDisplay[] seeds;
+
     public void OpenClose()
     {
         if(UIController.instance.theIC.gameObject.activeSelf == false)
         {
             gameObject.SetActive(!gameObject.activeSelf);
+
+            if(gameObject.activeSelf == true)
+            {
+                foreach(ShopSeedDisplay seed in seeds)
+                {
+                    seed.UpdateDisplay();
+                }
+            }
         }
     }    
 }
---
+++ Farming Game/Assets/Scripts/ShopSeedDisplay.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopSeedDisplay : MonoBehaviour
+{
+    public CropController.CropType crop;
+    
+    public Image seedImage;
+    public TMP_Text seedAmount, priceText;
+
+    public void UpdateDisplay()
+    {
+        CropInfo info = CropController.instance.GetCropInfo(crop);
+
+        seedImage.sprite = info.seedType;
+        seedAmount.text = "x" + info.seedAmount;
+
+        priceText.text = "$" + info.seedPrice + " each";
+    }
+
+    public void BuySeed(int amount)
+    {
+
+    }
+
+}
